853f5199cc7a9aefb1564af156242438e63efe45
F I R E <firethedev@gmail.com>
2021-07-26 02:40:00 +0600

    home route updated

--- server/server.js
+++ server/server.js
@@ -28,19 +28,24 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 // endpoints
-// home api
-app.get('/', (req, res) => {
-    res.send("Sup, bois!");
-})
-
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// home api
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send("Sup, bois!");
+    })
+}
 
 // custom error handler middlewares
 app.use(notFound);
